6a2d77d410f140daa1f086faaef9ff738f67e540
fifty-thirty <fiftythirty@proton.me>
2023-02-19 16:18:26 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,19 @@
+# Not Gonna Click It
+
+## What is this?
+Not Gonna Click It is a concept for a skill based minting procedure for digital collectibles.
+
+Users can only collect a token once they have achieved a score of 50.
+
+This means that they must either already posess the skills required, and make them a good fit for the community and likely to hang around, or they will have to spend the time building up their skills, at which point they will want a return on their time.
+
+Both outcomes could reduce the number of people collecting tokens purely for profit and help community builders start their journey with a strong core of people.
+
+## What do I get if I collect one?
+A piece of generative art.
+Proof that you have better than average clicking skills.
+Future access to a competitive and rewarding game that requires a token to enter.
+
+## Who made this?
+The 3D models were created by XCOPY and Neonglitch86.
+The game was created by 5030 studios, rekt5030 and bwcaboy.
---
+++ src/script.js
@@ -0,0 +1,876 @@
+import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import Web3Modal from "web3modal";
+import { Contract, providers, utils } from "ethers";
+
+var web3ModalRef = new Web3Modal({
+    network: "mainnet",
+    providerOptions: {},
+    disableInjectedProvider: false,
+});
+
+/**
+ * Contract Details
+ */
+export const NFT_CONTRACT_ADDRESS = '0x17C114fA62e67826A3e11105d82036dB010236A4';
+export const abi = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "baseURI",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "approved",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Approval",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "ApprovalForAll",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "previousOwner",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "OwnershipTransferred",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "indexed": true,
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "Transfer",
+    "type": "event"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "fallback"
+  },
+  {
+    "inputs": [],
+    "name": "_paused",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "_price",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "approve",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getApproved",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      }
+    ],
+    "name": "isApprovedForAll",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "maxTokenIds",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "mint",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "name",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "ownerOf",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "renounceOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bytes",
+        "name": "data",
+        "type": "bytes"
+      }
+    ],
+    "name": "safeTransferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "operator",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "approved",
+        "type": "bool"
+      }
+    ],
+    "name": "setApprovalForAll",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bool",
+        "name": "val",
+        "type": "bool"
+      }
+    ],
+    "name": "setPaused",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "bytes4",
+        "name": "interfaceId",
+        "type": "bytes4"
+      }
+    ],
+    "name": "supportsInterface",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "symbol",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenByIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "tokenIds",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenOfOwnerByIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "totalSupply",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "from",
+        "type": "address"
+      },
+      {
+        "internalType": "address",
+        "name": "to",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "transferFrom",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "newOwner",
+        "type": "address"
+      }
+    ],
+    "name": "transferOwnership",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "withdraw",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "stateMutability": "payable",
+    "type": "receive"
+  }
+]
+
+/**
+ * publicMint: Mint an NFT
+ */
+const publicMint = async () => {
+    try {
+      console.log("Public mint");
+      // We need a Signer here since this is a 'write' transaction.
+      const signer = await getProviderOrSigner(true);
+      // Create a new instance of the Contract with a Signer, which allows
+      // update methods
+      const nftContract = new Contract(NFT_CONTRACT_ADDRESS, abi, signer);
+      // call the mint from the contract to mint the NCGI
+      const tx = await nftContract.mint({
+        // value signifies the cost of one NCGI which is "0.0069" eth.
+        // We are parsing `0.0069` string to ether using the utils library from ethers.js
+        value: utils.parseEther("0.0069"),
+      });
+      setLoading(true);
+      // wait for the transaction to get mined
+      await tx.wait();
+      setLoading(false);
+      window.alert("You successfully minted a token!");
+    } catch (err) {
+      console.error(err);
+    }
+};
+
+ /**
+  * Returns a Provider or Signer object representing the Ethereum RPC with or without the
+  * signing capabilities of metamask attached
+  *
+  * A `Provider` is needed to interact with the blockchain - reading transactions, reading balances, reading state, etc.
+  *
+  * A `Signer` is a special type of Provider used in case a `write` transaction needs to be made to the blockchain, which involves the connected account
+  * needing to make a digital signature to authorize the transaction being sent. Metamask exposes a Signer API to allow your website to
+  * request signatures from the user using Signer functions.
+  * @param {*} needSigner - True if you need the signer, default false otherwise
+ */
+ const getProviderOrSigner = async (needSigner = false) => {
+    // Connect to Metamask
+    // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
+    const provider = await web3ModalRef.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    // If user is not connected to the Mainnet network, let them know and throw an error
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 1) {
+      window.alert("Change the network to Mainnet");
+      throw new Error("Change network to Mainnet");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+};
+
+/**
+ * Base
+ */
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Objects
+ */
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Raycaster
+ */
+const raycaster = new THREE.Raycaster()
+
+/**
+ * Mouse
+ */
+const mouse = new THREE.Vector2()
+
+window.addEventListener('mousemove', (_event) => {
+    var rect = renderer.domElement.getBoundingClientRect();
+    mouse.x = ( ( _event.clientX - rect.left ) / ( rect.right - rect.left ) ) * 2 - 1;
+    mouse.y = - ( ( _event.clientY - rect.top ) / ( rect.bottom - rect.top) ) * 2 + 1;
+})
+
+window.addEventListener('click', (_event) => {
+    const objectsToCheck = []
+    targets.forEach(item => objectsToCheck.push(item.model))
+
+    // Cast a ray
+    raycaster.setFromCamera(mouse, camera)
+
+    var intersects = raycaster.intersectObjects(objectsToCheck)
+
+    if(intersects.length) {
+        targets[intersects[0].object.name].clicked()
+    }
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 1
+scene.add(camera)
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas,
+    antialias: true
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Lights
+ */
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.3)
+scene.add(ambientLight)
+
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.7)
+directionalLight.position.set(1, 2, 3)
+scene.add(directionalLight)
+
+/**
+ * Models
+ */
+const gltfLoader = new GLTFLoader()
+
+let wagmi = null
+gltfLoader.load(
+    'models/wagmi.glb',
+    (gltf) => {
+        wagmi = gltf.scene
+        wagmi.traverse((child, i) => {
+            child.name = 'wagmi'
+        })
+    }
+)
+
+let ngmi = null
+gltfLoader.load(
+    'models/ngmi.glb',
+    (gltf) => {
+        ngmi = gltf.scene
+        ngmi.traverse((child) => {
+            child.name = 'ngmi'
+        })
+    }
+)
+
+/**
+ * Game Objects
+ */
+class Game {
+    #highscore = 0
+    #score = 0
+    constructor() {
+        this.highscore = 0
+        this.score = 0
+    }
+
+    getHighscore() {
+        return this.highscore
+    }
+
+    getScore() {
+        return this.score
+    }
+
+    resetScore() {
+        this.score = 0
+    }
+
+    increaseScore() {
+        this.score += 1
+    }
+
+    setNewHighscore() {
+        this.highscore = this.score
+        if(this.highscore >= 50) {
+            this.awakeMintButton()
+        }
+    }
+
+    updateScore() {
+        document.getElementById('score').innerText = 'score: ' + this.getScore()
+    }
+
+    gameOver() {
+        if(this.score > this.highscore) {
+            this.setNewHighscore()
+            console.log(this.highscore)
+            document.getElementById('highscore').innerText = 'Highscore: ' + this.highscore
+        }
+        this.resetScore()
+        targets.forEach(item => scene.remove(item.model))
+        targets.length = 0
+    }
+
+    awakeMintButton() {
+        let b = document.getElementById('mint')
+        b.disabled = false
+        b.style.pointerEvents = 'all'
+        b.onclick = () => { publicMint() }
+    }
+}
+let game = new Game()
+
+class Target {
+    constructor() {
+        this.model = Math.random() > 0.8 ? wagmi.clone() : ngmi.clone()
+        this.type = this.model.children[0].name
+        this.model.position.x = Math.random() * 1 - 0.5
+        this.model.position.y = Math.random() * 1 - 0.7
+        this.scale = 1
+        this.rate = Math.random() / 200
+        this.model.name = (targets.length).toString()
+        this.model.traverse((child) => {
+            child.name = (targets.length).toString()
+        })
+        this.rotation = (Math.random() * 16) + 16
+    }
+
+    createNewTarget() {
+        targets.push(new Target())
+        scene.add(targets[targets.length - 1].model)
+    }
+
+    randomPosition() {
+        if(Math.random() > 0.8 || targets.length == 1) {
+            this.createNewTarget()
+        }
+        this.scale = 1
+        this.model.position.x = Math.random() * 1 - 0.5
+        this.model.position.y = Math.random() * 1 - 0.7
+    }
+
+    clicked() {
+        if(this.type == 'wagmi') {
+            // LOSE
+            game.gameOver()
+        } else {
+            // SCORE POINTS
+            game.increaseScore()
+            this.randomPosition()
+        }  
+    }
+
+    update() {
+        this.scale -= this.rate
+        this.model.scale.set(this.scale, this.scale, this.scale)
+        this.model.rotation.y += Math.PI / this.rotation
+        if(this.scale <= 0) {
+            // LOSE
+            if(this.type == 'ngmi') {
+                game.gameOver()
+            }
+            this.randomPosition()
+        }
+    }
+}
+
+const targets = []
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    if(wagmi && ngmi) {
+
+        game.updateScore()
+
+        if(targets.length === 0) {
+            targets.push(new Target())
+            scene.add(targets[targets.length - 1].model)
+        }
+
+        targets.forEach(item => item.update())
+
+    }
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
